7d3f53b7b9c5b00b92447d0ac8d24c7d2c2f2f56
henrique-zw <henrique.zwetsch@edu.pucrs.br>
2020-03-25 20:32:35 -0300

    Implementação do consrtutor e metodo listarTodas da classe GerenciadorCias

--- src/GerenciadorCias.java
+++ src/GerenciadorCias.java
@@ -1,4 +1,14 @@
 import java.util.ArrayList;
 public class GerenciadorCias {
     private ArrayList<CiaAerea> empresas;
+
+    public GerenciadorCias(){
+        this.empresas = new ArrayList<>();
+    }
+    public ArrayList<CiaAerea> listarTodas(){
+        ArrayList<CiaAerea> aux = new ArrayList<>();
+        for(int pos = 0; pos < empresas.size(); pos++)
+            aux.add(empresas.get(pos));
+        return aux;
+    }
 }
