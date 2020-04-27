ec76ed327dec5b39d0640d542098cc1e08f620ad
henrique-zw <henrique.zwetsch@edu.pucrs.br>
2020-04-27 16:52:09 -0300

    Implementação do construtor da classe GerenciadorAeronave, método listarTodas que retorna um ArrayList com todas aeronaves, método adicionar para adicionar novas aeronaves, método removeAeronave que remove determinada aeronave, sizeAeronaves que retorna a quantidade de aeronaves e um método que busca uma aeronave pelo codigo e retorna ela caso exista.

--- src/GerenciadorAeronave.java
+++ src/GerenciadorAeronave.java
@@ -1,4 +1,35 @@
 import java.util.ArrayList;
 public class GerenciadorAeronave {
     private ArrayList<Aeronave> aeronaves;
+
+    public GerenciadorAeronave(){
+        this.aeronaves = new ArrayList<>();
+    }
+    public ArrayList<Aeronave> listarTodas(){
+        ArrayList<Aeronave> aux = new ArrayList<>();
+        for(Aeronave aviao : aeronaves)
+            aux.add(aviao);
+        return aux;
+    }
+    public void adicionar(Aeronave aviao){
+        aeronaves.add(aviao);
+    }
+    public void removeAreonave(Aeronave aviao){
+        aeronaves.remove(aviao);
+    }
+    public int sizeAeronaves(){
+        return aeronaves.size();
+    }
+    public Aeronave buscaPorCodigo(String cod){
+        for(Aeronave aviao : aeronaves){
+            if(aviao.getCodigo().equals(cod)){
+                return aviao;
+            }
+            else {
+                System.out.println("aeronave não encontrada");
+                return null;
+            }
+        }
+        return null;
+    }
 }
