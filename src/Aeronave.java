e310ef9c80238856fa760fb6f91a6103cad2b74d
henrique-zw <henrique.zwetsch@edu.pucrs.br>
2020-04-27 17:08:18 -0300

    Implementação do método construtor da classe Aeronave

--- src/Aeronave.java
+++ src/Aeronave.java
@@ -3,6 +3,11 @@ public class Aeronave {
     private String descricao;
     private int capacidade;
 
+    public Aeronave(String codigoIn, String descricaoIn, int capacidadeIn){
+        codigo = codigoIn;
+        descricao = descricaoIn;
+        capacidade = capacidadeIn;
+    }
     public String getCodigo() {
         return codigo;
     }
